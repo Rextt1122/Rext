d7809a3c6694a9e83a18a9ee89b7092572e894a4
Biyann <fabianhendriyansah@gmail.com>
2025-11-30 11:32:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const toggleBtn = document.getElementById("themeToggle");
 
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+    toggleBtn.textContent = "☀️"; 
+}
+
 toggleBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark");
 
     if (document.body.classList.contains("dark")) {
-        toggleBtn.textContent = "☀️";
+        toggleBtn.textContent = "☀️"; 
         localStorage.setItem("theme", "dark");
     } else {
         toggleBtn.textContent = "🌙";
@@ -12,14 +17,20 @@ toggleBtn.addEventListener("click", () => {
     }
 });
 
-if (localStorage.getItem("theme") === "dark") {
-    document.body.classList.add("dark");
-    toggleBtn.textContent = "☀️";
-}
+const desainList = [
+  { img: "img/Waguri.png"},
+  { img: "img/Dream Journey.png"},
+  { img: "img/Tenka.png"},
+];
 
-const menuBtn = document.getElementById("menuBtn");
-const mobileNav = document.getElementById("mobileNav");
+const gallery = document.getElementById("desain-gallery");
 
-menuBtn.addEventListener("click", () => {
-    mobileNav.classList.toggle("show-nav");
+desainList.forEach((d, index) => {
+    const item = document.createElement("div");
+    item.className = "gallery-item";
+    item.setAttribute("data-aos", "fade-up");       
+    item.setAttribute("data-aos-duration", "1000"); 
+    item.setAttribute("data-aos-delay", index * 200); 
+    item.innerHTML = `<img src="${d.img}">`;
+    gallery.appendChild(item);
 });
