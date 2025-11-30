ef75a98890326cd4a2cb389f0e319c385ca400db
Biyann <fabianhendriyansah@gmail.com>
2025-11-30 12:31:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -25,10 +25,10 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 const desainList = [
-    { img: "img/Waguri.png" },
-    { img: "img/Dream Journey.png" }
-    { img: "img/Tenka.png" }
-    { img: "img/Miyabi.png" }
+  { img: "img/Waguri.png" },
+  { img: "img/DreamJourney.png" },
+  { img: "img/Tenka.png" },
+  { img: "img/Miyabi.png" }
 ];
 
 function loadDesain() {
