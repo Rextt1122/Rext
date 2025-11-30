d8bac4f90d66d6c7eb4e54bb5be935c9194d8055
Biyann <fabianhendriyansah@gmail.com>
2025-11-30 10:54:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const toggleBtn = document.getElementById("themeToggle");
 
 if (localStorage.getItem("theme") === "dark") {
     document.body.classList.add("dark");
-    toggleBtn.textContent = "☀️"; 
+    toggleBtn.textContent = "☀️";	
 }
 
 toggleBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark");
 
     if (document.body.classList.contains("dark")) {
-        toggleBtn.textContent = "☀️"; 
+        toggleBtn.textContent = "☀️";	
         localStorage.setItem("theme", "dark");
     } else {
         toggleBtn.textContent = "🌙";
@@ -17,20 +17,37 @@ toggleBtn.addEventListener("click", () => {
     }
 });
 
+const menuToggle = document.querySelector('.menu-toggle');
+const headerPill = document.querySelector('.header-pill');
+
+if(menuToggle) {
+    menuToggle.addEventListener('click', () => {
+        headerPill.classList.toggle('active');
+    });
+}
+
+document.querySelectorAll('.pill-nav a').forEach(link => {
+    link.addEventListener('click', () => {
+        headerPill.classList.remove('active');
+    });
+});
+
 const desainList = [
-  { img: "img/Waguri.png"},
-  { img: "img/Dream Journey.png"},
-  { img: "img/Tenka.png"},
+  { img: "img/Waguri.png"},
+  { img: "img/Dream Journey.png"},
+  { img: "img/Tenka.png"},
 ];
 
 const gallery = document.getElementById("desain-gallery");
 
-desainList.forEach((d, index) => {
-    const item = document.createElement("div");
-    item.className = "gallery-item";
-    item.setAttribute("data-aos", "fade-up");       
-    item.setAttribute("data-aos-duration", "1000"); 
-    item.setAttribute("data-aos-delay", index * 200); 
-    item.innerHTML = `<img src="${d.img}">`;
-    gallery.appendChild(item);
-});
+if (gallery) {
+    desainList.forEach((d, index) => {
+        const item = document.createElement("div");
+        item.className = "gallery-item";
+        item.setAttribute("data-aos", "fade-up");       
+        item.setAttribute("data-aos-duration", "1000"); 
+        item.setAttribute("data-aos-delay", index * 200); 
+        item.innerHTML = `<img src="${d.img}">`;
+        gallery.appendChild(item);
+    });
+}
