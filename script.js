eb3245729b9be81034beccf861c38504a03e1107
Biyann <fabianhendriyansah@gmail.com>
2025-11-30 10:33:57 +0700

    Refactor theme toggle and add gallery functionality

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-const themeToggle = document.getElementById("themeToggle");
-const hamburgerBtn = document.getElementById("hamburgerBtn");
-const mobileNav = document.getElementById("mobileNav");
+const toggleBtn = document.getElementById("themeToggle");
 
-hamburgerBtn.addEventListener("click", () => {
-    mobileNav.style.display =
-        mobileNav.style.display === "flex" ? "none" : "flex";
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+    toggleBtn.textContent = "☀️"; 
+}
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        toggleBtn.textContent = "☀️"; 
+        localStorage.setItem("theme", "dark");
+    } else {
+        toggleBtn.textContent = "🌙";
+        localStorage.setItem("theme", "light");
+    }
 });
 
-themeToggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
+const desainList = [
+  { img: "img/Waguri.png"},
+  { img: "img/Dream Journey.png"},
+  { img: "img/Tenka.png"},
+];
+
+const gallery = document.getElementById("desain-gallery");
+
+desainList.forEach((d, index) => {
+    const item = document.createElement("div");
+    item.className = "gallery-item";
+    item.setAttribute("data-aos", "fade-up");       
+    item.setAttribute("data-aos-duration", "1000"); 
+    item.setAttribute("data-aos-delay", index * 200); 
+    item.innerHTML = `<img src="${d.img}">`;
+    gallery.appendChild(item);
 });
