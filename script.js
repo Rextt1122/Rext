9922ff55bfd0cb815f9a7d3f42b15ee692668c65
Biyann <fabianhendriyansah@gmail.com>
2025-11-30 10:29:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,12 @@
-const toggleBtn = document.getElementById("themeToggle");
+const themeToggle = document.getElementById("themeToggle");
+const hamburgerBtn = document.getElementById("hamburgerBtn");
+const mobileNav = document.getElementById("mobileNav");
 
-if (localStorage.getItem("theme") === "dark") {
-    document.body.classList.add("dark");
-    toggleBtn.textContent = "☀️"; 
-}
-
-toggleBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-
-    if (document.body.classList.contains("dark")) {
-        toggleBtn.textContent = "☀️";
-        localStorage.setItem("theme", "dark");
-    } else {
-        toggleBtn.textContent = "🌙"; 
-        localStorage.setItem("theme", "light");
-    }
+hamburgerBtn.addEventListener("click", () => {
+    mobileNav.style.display =
+        mobileNav.style.display === "flex" ? "none" : "flex";
 });
 
-const desainList = [
-  { img: "img/Waguri.png"},
-  { img: "img/Dream Journey.png"},
-  { img: "img/Tenka.png"},
-];
-
-const gallery = document.getElementById("desain-gallery");
-
-desainList.forEach((d, index) => {
-    const item = document.createElement("div");
-    item.className = "gallery-item";
-    item.setAttribute("data-aos", "fade-up");       
-    item.setAttribute("data-aos-duration", "1000"); 
-    item.setAttribute("data-aos-delay", index * 200); 
-    item.innerHTML = `<img src="${d.img}">`;
-    gallery.appendChild(item);
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
 });
